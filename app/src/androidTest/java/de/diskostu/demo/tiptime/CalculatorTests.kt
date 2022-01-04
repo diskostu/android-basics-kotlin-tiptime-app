2107973bb66d88459b41217159178d1b2563f3fa
diskostu <denny.kluge@gmail.com>
2022-01-04 21:15:03 +0100

    switch to JUnit5

--- app/src/androidTest/java/de/diskostu/demo/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/de/diskostu/demo/tiptime/CalculatorTests.kt
@@ -4,22 +4,23 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.matcher.ViewMatchers.*
+import de.mannodermaus.junit5.ActivityScenarioExtension
 import org.hamcrest.Matchers.containsString
-import org.junit.Rule
-import org.junit.Test
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 
 class CalculatorTests {
 
-    @get:Rule
-    val activity = ActivityScenarioRule(MainActivity::class.java)
+    @JvmField
+    @RegisterExtension
+    val scenarioExtension = ActivityScenarioExtension.launch<MainActivity>()
 
     @Test
     fun calculate_tip_1_rounded() {
         onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("54.00"))
         onView(withId(R.id.option_twenty_percent)).perform(click())
+        onView(withId(R.id.round_up_switch)).check(matches(isChecked()))
 
         onView(withId(R.id.button_calculate)).perform(click())
 
@@ -31,8 +32,8 @@ class CalculatorTests {
     fun calculate_tip_1_not_rounded() {
         onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("54.00"))
         onView(withId(R.id.option_twenty_percent)).perform(click())
+        onView(withId(R.id.round_up_switch)).check(matches(isChecked()))
         onView(withId(R.id.round_up_switch)).perform(click())
-
 
         onView(withId(R.id.button_calculate)).perform(click())
 
